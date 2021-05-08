77235531cd91d5ea39750878606a77f799f84f5d
rroyalty <rroyalty@gmail.com>
2021-05-08 11:06:59 -0400

    Logo test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import fullLogo from './fullLogo.png'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={fullLogo} className="App-logo" alt="logo" />
         <p>
           WELCOME TO BANDWIDTH
         </p>
