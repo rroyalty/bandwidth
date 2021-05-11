3387f3f186926af99b01b7dbf30a86a7e3916716
rroyalty <rroyalty@gmail.com>
2021-05-11 15:14:38 -0400

    server's listening

--- server/server.ts
+++ server/server.ts
@@ -1,32 +1,38 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const PORT = process.env.PORT || 3000;
-const db = require("./models");
-const router = require('./routes/router');
+// const express = require("express");
+// const path = require("path");
+// const app = express();
 
-db.sequelize.sync({force: false});
+// const db = require("./models");
+// const router = require('./routes/router');
+
+// import path from 'path'
+const express = require("express");
+const PORT =  3001;
+const app = express();
+
+// db.sequelize.sync({force: false});
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "../client/dist/bench-strength")));
 // }
 
-app.use(router(db));
+// app.use(router(db));
 
 // app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/dist/bench-strength/index.html"));
+//   res.sendFile(path.join(__dirname, "../client/public/index.html"));
 // });
 
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
 
 app.listen(PORT, function() {
