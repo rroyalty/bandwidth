6a7c499e5470eb21f85ff91c194aef954b22beb9
Toni Powell <tonipow3ll@gmail.com>
2021-05-15 11:10:16 -0400

    redux slice set up

--- client/src/views/Profile/EditProfile.tsx
+++ client/src/views/Profile/EditProfile.tsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+import { profileUpdated } from './profileSlice'
 export const EditProfile = () => {
     // intention status (available / unavailable)
     // band name
     // phone
     // email
     // location
-
+    const [displayName, setDisplayName] = useState('')
     const [status, setStatus] = useState('')
     const [bandName, setBandName] = useState('')
     const [phone, setPhone] = useState('')
@@ -16,7 +17,10 @@ export const EditProfile = () => {
     const [location, setLocation] = useState('')
 
     const dispatch = useDispatch();
+    // not sure if i need the below quite yet
+    // const history = useHistory();
 
+    const onDisplayNameChanged = (e: { target: { value: React.SetStateAction<string>; }; }) => setDisplayName(e.target.value)
     const onStatusChanged = (e: { target: { value: React.SetStateAction<string>; }; }) => setStatus(e.target.value)
     const onBandNameChanged = (e: { target: { value: React.SetStateAction<string>; }; }) => setBandName(e.target.value)
     const onPhoneChanged = (e: { target: { value: React.SetStateAction<string>; }; }) => setPhone(e.target.value)
@@ -25,8 +29,61 @@ export const EditProfile = () => {
     
     const onUpdateProfileClicked = () => {
         if (status || bandName || phone || email || location) {
-            // dispatch(profileUpdated({status, phone, email, location}))
-            // create profileUpdated in profileSlice.tsx
+            dispatch(profileUpdated({displayName, status, phone, email, location}))
+            // what am I pushing to history?
+            // history.push(``)
         }
     }
+    return (
+        <section>
+            <h2>Edit Profile</h2>
+            <form>
+                <label htmlFor="editProfile">Edit Profile</label>
+                <input 
+                type='text'
+                id='displayName'
+                placeholder='Display Name'
+                value={displayName}
+                onChange={onDisplayNameChanged}
+                />
+                 <input 
+                type='text'
+                id='displayName'
+                placeholder='Available, or Unavailable?'
+                value={status}
+                onChange={onStatusChanged}
+                />
+                <input 
+                type='text'
+                id='bandName'
+                placeholder='Band Name'
+                value={bandName}
+                onChange={onBandNameChanged}
+                />
+                <input 
+                type='text'
+                id='phone'
+                placeholder='Display Name'
+                value={phone}
+                onChange={onPhoneChanged}
+                />
+                <input 
+                type='text'
+                id='email'
+                placeholder='Display Name'
+                value={email}
+                onChange={onEmailChanged}
+                />
+                <input 
+                type='text'
+                id='location'
+                placeholder='Display Name'
+                value={location}
+                onChange={onLocationChanged}
+                />
+            </form>
+            <button type="button" onClick={onUpdateProfileClicked}>Save</button>
+            <button type="button">Discard</button>
+        </section>
+    )
 }
--- client/src/views/Profile/profileSlice.tsx
+++ client/src/views/Profile/profileSlice.tsx
@@ -5,10 +5,45 @@ import { createSlice, nanoid } from '@reduxjs/toolkit'
 // phone # 888-888-8888
 // email = email from auth0
 const initialState = [
-    { id: '1', title: 'First Post!', content: 'Hello!' },
-    { id: '2', title: 'Second Post', content: 'Another post!' }
+    { id: nanoid(), displayName: 'Toni Powell', bandName: 'Hello!', phone: '888-888-8888', email: 'email@email.com', location: 'Boston, MA'},
+    // { id: '2', title: 'Second Post', content: 'Another post!' }
   ]
 
-//   const profileSlice = createSlice({
-  //profileUpdated get's called here 
-//   })
+  const profileSlice = createSlice({
+//   profileUpdated get's called here 
+name: 'profile',
+initialState,
+reducers: {
+    profileAdded: {
+        reducer(state,action) {
+            state.push(action.payload)
+        }, 
+        // payload here (mini-model)
+        prepare(displayName, bandName, phone, email, location) {
+            return {
+                payload: {
+                    // id: nanoid(),
+                    displayName, 
+                    bandName,
+                    phone,
+                    email, 
+                    location
+                }
+            }
+        }
+    },
+    profileUpdated(state, action){
+        const {id, displayName, bandName, phone, email, location} = action.payload
+        const existingProfile = state.find(profile => profile.id === id)
+        if (existingProfile) {
+            existingProfile.displayName = displayName,
+            existingProfile.bandName = bandName,
+            existingProfile.phone = phone,
+            existingProfile.email = email,
+            existingProfile.location = location
+        }
+    }
+}
+})
+export const { profileAdded, profileUpdated } = profileSlice.actions
+export default profileSlice.reducer
