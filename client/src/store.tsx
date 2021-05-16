4820f4bc8a2ed497d857b158badbd8a194a177e5
rroyalty <rroyalty@gmail.com>
2021-05-16 10:09:24 -0400

    profileState

--- client/src/store.tsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import profileReducer from '../src/views/Profile/profileSlice'
-export default configureStore({
-  // switch statements will go in reducer file 
-  reducer: {
-    profile: profileReducer
-  },
-})
