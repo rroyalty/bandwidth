214013058eb82f3d6ec34698d4f8e11dbaa76015
Toni Powell <tonipow3ll@gmail.com>
2021-05-22 11:02:45 -0400

    userprofile stuff

--- client/src/views/UserProfile/UserProfile.tsx
+++ client/src/views/UserProfile/UserProfile.tsx
@@ -1,72 +1,71 @@
 import React, { useEffect, useState } from "react";
-
-import { Container, Button } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import './style.css';
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from 'react-router-dom';
-import { useSelector } from "react-redux";
-import axios from 'axios'
-import { RootState } from "../../redux/store";
+// import { useSelector } from "react-redux";
+// import axios from 'axios'
+// import { RootState } from "../../redux/store";
 import API from "../../utils/API";
+import CreateProfile from '../CreateProfile/CreateProfile'
 
 // ========================================================================================================
 // User Profile page - this pulls info from the DB, and compares a users logged in email, with info we have stored
 // ===============================================================================================================
 export interface IUser {
-    nickName: string,
-    firstName: string,
-    lastName: string,
-    intentionStatus: string,
-    location: string,
-    email: string,
-    phone: string,
-    blurb: string,
+  nickName: string,
+  firstName: string,
+  lastName: string,
+  intentionStatus: string,
+  location: string,
+  email: string,
+  phone: string,
+  blurb: string,
 }
 
 const UserProfile = () => {
   const userProfile: any = useAuth0();
 
-//  console.log(userProfile.user)
+  //  console.log(userProfile.user)
 
   const [user, setUser] = useState<IUser[]>([])
 
   const writeProfile = (res: any[]) => {
     const users: IUser[] = res.map((user) => {
-        return {
-            nickName: user.nickName,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            intentionStatus: user.intentionStatus,
-            location: user.location,
-            email: user.email,
-            phone: user.phone,
-            blurb: user.blurb
-        }
+      return {
+        nickName: user.nickName,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        intentionStatus: user.intentionStatus,
+        location: user.location,
+        email: user.email,
+        phone: user.phone,
+        blurb: user.blurb
+      }
     })
     console.log(users)
     setUser(users)
-    
-}
+
+  }
 
 
-useEffect(() => {
+  useEffect(() => {
     API.getUsers().then(res => {
-        const findUser = res.data;
-        const thisUser = findUser.filter((findUser: any) => userProfile.user.email === findUser.email)
-
-        // console.log(thisUser[0])
-        writeProfile(thisUser)
-        // dbUsers(res.data);
+      const findUser = res.data;
+      const thisUser = findUser.filter((findUser: any) => userProfile.user.email === findUser.email)
+      // console.log(thisUser[0])
+      writeProfile(thisUser)
+     userExists()
     })
-}, [])
+  }, [])
 
-  return (
-    <div className="paddingfix">
-{user.map((user) => {
-    return (
-        <Container  maxWidth="lg" >
+   const userExists = () => {
+     return (
+   <div className="paddingfix">
+      {user.map((user) => {
+        return (
+          <Container maxWidth="lg" >
             <h1>User Profile Page</h1>
-    
             <h1 key={user.nickName}>{user.nickName}</h1>
             <p key={user.firstName}>{user.firstName} {user.lastName}</p>
             <p key={user.intentionStatus}>{user.intentionStatus}</p>
@@ -74,11 +73,30 @@ useEffect(() => {
             <p key={user.email}>{user.email}</p>
             <p key={user.phone}>{user.phone}</p>
             <p key={user.blurb}>{user.blurb}</p>
+          </Container>
+        )
+      })}
+    </div>
+     )
+   } 
 
-    </Container>
-)
-})}
-</div>
+   const noUserExists = () => {
+     return(
+       <>
+       <Container>
+       <div className="paddingfix test">
+       <h1>hello</h1>
+       <Link className="paddingfix test" to="createprofile">TESTING LINK</Link>
+       <p>yoyoyo</p>
+       </div>
+       </Container>
+       </>
+     )
+   }
+  return (
+    <div className="paddingfix ">
+      {!user ? noUserExists() : userExists() }
+    </div>
   );
 };
 
