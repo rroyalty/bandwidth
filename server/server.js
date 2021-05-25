1ff0b6f12d759b93703e93219b917b199c72b457
rroyalty <rroyalty@gmail.com>
2021-05-24 21:04:15 -0400

    serverserser

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3020;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
 app.use(routes);
 
