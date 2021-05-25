a5187a3962a2f77a0d9e421378a3619a8cef8c44
rroyalty <rroyalty@gmail.com>
2021-05-25 12:25:18 -0400

    sequelizerc

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 module.exports = {
-
+  "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PW,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-
     "dialect": "mysql",
     "define": {
-        "timestamps": false
-      }
+      "timestamps": false
+    }
+  }
 }
