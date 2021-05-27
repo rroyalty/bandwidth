61388defacdf570a2dc812826223dc17909dde0b
Toni Powell <tonipow3ll@gmail.com>
2021-05-26 20:15:22 -0400

    landing immediately redirects to profile

--- client/src/views/Landing/loggedInLanding.tsx
+++ client/src/views/Landing/loggedInLanding.tsx
@@ -7,20 +7,37 @@ import CreateProfile from '../CreateProfile/CreateProfile';
 import ProtectedRoute from '../../auth/protected-route';
 import EditProfile from '../EditProfile/EditProfile'
 import PrevProfile from '../EditProfile/PrevProfile'
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 
 const LoggedInLanding: React.FC = (): JSX.Element => {
 
-    // window.location.href = "/userprofile"
+    const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        header: {
+            backgroundColor: `rgba(255, 255, 255, 0.4)`,
+            paddingTop: 100,
+            // paddingLeft: 50,
+            justifyContent: `center`,
+            alignItems: `center`,
+            textAlign: `center`,
+        },
+    })
+    )
+    const classes = useStyles();
     return (
         <>
+        
             <Switch>
                 <ProtectedRoute exact path="/tempprofile" component={TempProfile} />
                 <ProtectedRoute exact path="/prevprofile" component={PrevProfile} />
                 <ProtectedRoute exact path="/createprofile" component={CreateProfile} />
                 <ProtectedRoute exact path="/editprofile" component={EditProfile} />
-                <ProtectedRoute exact path="/userProfile" component={UserProfile} />
+                <ProtectedRoute exact path="/" component={UserProfile} />
                 <ProtectedRoute exact path="/find" component={Find} />
             </Switch>
+           <div className={classes.header}>
+               {/* <UserProfile /> */}
+           </div>
         </>
     );
 }
