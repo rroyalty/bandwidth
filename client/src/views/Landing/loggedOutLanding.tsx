6acf925c75d96432d6914f8c074a9fb5d3dcb547
rroyalty <rroyalty@gmail.com>
2021-05-28 11:16:27 -0400

    typo

--- client/src/views/Landing/loggedOutLanding.tsx
+++ client/src/views/Landing/loggedOutLanding.tsx
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 }));
 
 
-const LoggedOutLanding: React.FC = (props: any): JSX.Element => {
+const LoggedOutLanding: React.FC = (): JSX.Element => {
   
   let bgArray: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8]
 
